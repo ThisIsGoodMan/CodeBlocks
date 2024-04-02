b781b51f6aeae49a2d700f455f91dfffc2a348d3
Jason G <124753175+ThisIsGoodMan@users.noreply.github.com>
2024-04-02 12:36:53 -0700

    Added create CodeBlocks page
    Added basic input functionality to allow users to create CodeBlocks

--- codeblocks/src/app/blocks/create/page.tsx
+++ codeblocks/src/app/blocks/create/page.tsx
@@ -1,6 +1,21 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
 export default function BlockCreatePage() {
+     async function createBlock(formData: FormData) {
+        "use server"
+        const title= formData.get("title") as string;
+        const code = formData.get("code") as string;
+        const content = formData.get("content") as string;
+        // Insert data to database
+        await db.block.create({
+            data: { title, code, content, user: { connect: { id: userId } } }
+        });
+        redirect("/");
+    }
+
     return (
-        <form action="">
+        <form action={createBlock}>
             <h3>Create a Block</h3>
             <div className="flex flex-col gap-4">
 
